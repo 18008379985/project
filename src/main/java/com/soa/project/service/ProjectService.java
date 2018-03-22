1606598f186d2865f50647b462c26fe39487932a
18008379985 <yanhua.li@qq.com>
2018-03-22 22:19:45 +0800

    2018-03-22

--- src/main/java/com/soa/project/service/ProjectService.java
+++ src/main/java/com/soa/project/service/ProjectService.java
@@ -4,11 +4,15 @@ package com.soa.project.service;
 import com.soa.project.domain.Project;
 import com.soa.project.domain.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.UUID;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import java.util.*;
 
 @RestController
 public class ProjectService {
@@ -24,6 +28,40 @@ public class ProjectService {
     public List<Project> getProjectList(){
 
         return projectRepository.findAll();
+    }
+
+    /**
+     * search project by name
+     * return string
+     */
+    @RequestMapping(value = "/service/findProject",method = RequestMethod.POST)
+    public List<Project> findProjectByName(@RequestParam String projectName){
+
+        return projectRepository.findAll(new Specification<Project>() {
+            @Override
+            public Predicate toPredicate(Root<Project> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+
+                return cb.like(root.get("projectName"), projectName);
+
+            }
+        });
+    }
+
+    /**
+     * search project by type
+     * return string
+     */
+    @RequestMapping(value = "/service/findProjectByType",method = RequestMethod.POST)
+    public List<Project> findProjectByType(@RequestParam String projectType){
+
+        return projectRepository.findAll(new Specification<Project>() {
+            @Override
+            public Predicate toPredicate(Root<Project> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+
+                return cb.equal(root.get("typeCode"), projectType);
+
+            }
+        });
     }
 
 
--- src/main/resources/static/js/project.js
+++ src/main/resources/static/js/project.js
@@ -1,26 +1,120 @@
 
+function doSearchByType(val,id){
 
+    $("a").css("color","#888");
 
+    $(id).css("color","#FE984F");
 
-function doSearch(value){
-    // $('#DataGrid').datagrid('load',{PROCESS_NAME: $('#searchStr').val()});
-    alert('You input: ' + value);
+    $.post('/service/findProjectByType',{projectType:val},function(data) {
 
-
-
-    $.post('GetData.jsp',{PROCESS_NAME:$('#searchStr').val()},function(result){
-
-
-        if (result)
-        {
-            $('#DataGrid').datagrid('loadData',result);
-
-        } else
-        {
-            $.messager.alert("操作提示", "查询失败!","info");
-        }
+        if (data) {$('#DataGrid').datagrid('loadData', data);} else{showMessage("没有查询到数据！");}
 
     },'json');
 
+
+
+}
+
+function doSearchByName(val){
+
+    $.post('/service/findProject',{projectName:val},function(data) {
+
+        if (data) {$('#DataGrid').datagrid('loadData', data);} else{showMessage("没有查询到数据！");}
+
+    },'json');
+}
+
+function showMessage(info){
+    $.messager.show({
+        title:'提示信息',
+        msg:info,
+        timeout:5000,
+        showType:'slide'
+    });
+}
+
+
+
+    function clearForm(){ $('#editForm').form('clear');}
+
+
+function doSave() {
+
+    $('#editForm').form('submit', {
+
+        url : 'SetData.jsp',
+
+        onSubmit : function() {
+
+            return $(this).form('enableValidation').form('validate');
+
+        },
+
+        success : function(data) {
+
+            if(data=="true"){
+
+                $.messager.alert("操作提示", "操作成功！","info");
+
+                $('#DataGrid').datagrid('reload');
+
+            }else{
+
+                $.messager.alert("操作提示", "操作失败！","error");
+            }
+        }
+    });
+}
+
+
+
+function doRemove()
+
+{
+    var sys_id = "";
+
+    var row = $('#DataGrid').datagrid('getSelections');
+
+    for(var i=0; i<row.length; i++)
+    {
+        sys_id += row[i].PROCESS_ID +",";
+    }
+
+    sys_id = sys_id.substr(0,sys_id.length-1); //去掉字符串最后面的逗号；
+
+    if (row.length>0)
+    {
+        $.messager.confirm('操作提示','确定要删除所选择的记录吗?',function(r){
+
+            if (r){
+
+                $.post('RemoveData.jsp',{PROCESS_ID:sys_id},function(result){
+
+
+                    if (result)
+                    {
+                        $('#DataGrid').datagrid('reload');    // reload the user data
+                    } else
+                    {
+                        $.messager.alert("操作提示", "删除失败!","info");
+                    }
+
+                },'json');
+
+            } // END IF (r)
+
+        });  // END  $.messager.confirm
+
+    } // END  if (row.length>0)
+
+} // END function doRemove
+
+
+//表格选择行记录赋值给表单
+function getSelected()
+{
+    var row = $('#DataGrid').datagrid('getSelected');
+
+    if (row){ $('#editForm').form('load', row)}
 }
 
